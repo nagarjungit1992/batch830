5d76b3842280b67e7dcd1d0d47876348387a187b
nagarjungit1992 <nagarjun101992@gmail.com>
2020-01-29 08:36:59 +0530

    user name added

---
+++ src/org/atos/test/login/CourseDetails.java
@@ -0,0 +1,24 @@
+package org.atos.test.login;
+	
+	public class CourseDetails {
+		// method decleration
+		private void javaCourse() {
+			// This is method
+			System.out.println("java course started at jan 21 ");
+		}
+
+		// method decleration
+		private void oracleCourse() {
+			System.out.println("oracle course not started   ");
+		}
+
+		public static void main(String[] args) {
+	      //classname objnme =new Classname();
+			CourseDetails  d =new CourseDetails();//object create
+		//objname.methodnaame();
+			d.javaCourse();//method call
+			d.oracleCourse();
+		}
+	}
+
+}
